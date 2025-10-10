bb219a0be0812934f221cf9132ce8a4fe510eabf
Harshvardhan <harshvardhan.sikarwar@badkultechnology.com>
2025-10-10 15:09:28 +0530

    experimental feature to solve the zoom issue - likely to fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import { Poppins, Barlow } from "next/font/google";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { ReduxProvider } from "@/lib/redux-provider";
 import HydratedAuth from "@/components/AuthLoader";
 import { Toaster } from "@/components/ui/toaster";
+import { usePathname } from "next/navigation";
 
-export const metadata = {
-  title: "Ragir - Organizer Dashboard",
-  description: "Travel organizer dashboard for managing trips and analytics",
-};
+// export const metadata = {
+//   title: "Ragir - Organizer Dashboard",
+//   description: "Travel organizer dashboard for managing trips and analytics",
+// };
 
 const barlow = Barlow({
   variable: "--font-barlow",
@@ -24,11 +27,17 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
+ 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+
+// if the path is prelaunch, add a class to the html element
+const pathname = usePathname();
+const isPrelaunch = pathname.startsWith("/prelaunch");
+
   return (
-    <html lang="en" className={poppins.variable}>
+    <html lang="en" className={`poppins.variable ${isPrelaunch ? "prelaunch-root" : ""}`}>
       <body className="font-poppins antialiased">
         <ReduxProvider>
           <HydratedAuth>
