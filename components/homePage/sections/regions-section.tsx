21bdc4f3a751e34020e102286388ae8d95f6d5f7
ankitjec8389 <ankitjec8389@gmail.com>
2026-01-05 19:17:30 +0530

    Update regions-section.tsx

--- components/homePage/sections/regions-section.tsx
+++ components/homePage/sections/regions-section.tsx
@@ -5,24 +5,24 @@ import Image from "next/image";
 import Link from "next/link";
 
 const domesticRegions = [
-  { name: "North India", icon: <Image src="/north-india-region.png" alt="North India" width={80} height={80} /> },
-  { name: "South India", icon: <Image src="/south-india-region.png" alt="North India" width={80} height={80} /> },
-  { name: "West India", icon: <Image src="/west-india-region.png" alt="North India" width={80} height={80} /> },
-  { name: "Central India", icon: <Image src="/central-india-region.png" alt="North India" width={80} height={80} /> },
-  { name: "East India", icon: <Image src="/east-india-region.png" alt="North India" width={80} height={80} /> },
-  { name: "North-East India", icon: <Image src="/north-east-india-region.png" alt="North India" width={80} height={80} /> },
+  { name: "North India", id: "north_india", icon: <Image src="/north-india-region.png" alt="North India" width={80} height={80} /> },
+  { name: "South India", id: "south_india", icon: <Image src="/south-india-region.png" alt="North India" width={80} height={80} /> },
+  { name: "West India", id: "west_india", icon: <Image src="/west-india-region.png" alt="North India" width={80} height={80} /> },
+  { name: "Central India", id: "central_india", icon: <Image src="/central-india-region.png" alt="North India" width={80} height={80} /> },
+  { name: "East India", id: "east_india", icon: <Image src="/east-india-region.png" alt="North India" width={80} height={80} /> },
+  { name: "North-East India", id: "north-east_india", icon: <Image src="/north-east-india-region.png" alt="North India" width={80} height={80} /> },
 ];
 
 const internationalRegions = [
-  { name: "South Asia", icon: <Image src="/south-asia-region.png" alt="North India" width={80} height={80} /> },
-  { name: "South-East Asia", icon: <Image src="/south-east-asia-region.png" alt="North India" width={80} height={80} /> },
-  { name: "East Asia", icon: <Image src="/east-asia-region.png" alt="North India" width={80} height={80} /> },
-  { name: "Middle East", icon: <Image src="/middle-east-region.png" alt="North India" width={80} height={80} /> },
-  { name: "Europe", icon: <Image src="/europe-region.png" alt="North India" width={80} height={80} /> },
-  { name: "Africa", icon: <Image src="/africa-region.png" alt="North India" width={80} height={80} /> },
-  { name: "North America", icon: <Image src="/north-america-region.png" alt="North India" width={80} height={80} /> },
-  { name: "South America", icon: <Image src="/south-america-region.png" alt="North India" width={80} height={80} /> },
-  { name: "Oceania", icon: <Image src="/oceania-region.png" alt="North India" width={80} height={80} /> },
+  { name: "South Asia", id: "south_asia", icon: <Image src="/south-asia-region.png" alt="North India" width={80} height={80} /> },
+  { name: "South-East Asia", id: "south_east_asia", icon: <Image src="/south-east-asia-region.png" alt="North India" width={80} height={80} /> },
+  { name: "East Asia", id: "east_asia", icon: <Image src="/east-asia-region.png" alt="North India" width={80} height={80} /> },
+  { name: "Middle East", id: "middle_east", icon: <Image src="/middle-east-region.png" alt="North India" width={80} height={80} /> },
+  { name: "Europe", id: "europe", icon: <Image src="/europe-region.png" alt="North India" width={80} height={80} /> },
+  { name: "Africa", id: "africa", icon: <Image src="/africa-region.png" alt="North India" width={80} height={80} /> },
+  { name: "North America", id: "north_america", icon: <Image src="/north-america-region.png" alt="North India" width={80} height={80} /> },
+  { name: "South America", id: "south_america", icon: <Image src="/south-america-region.png" alt="North India" width={80} height={80} /> },
+  { name: "Oceania", id: "oceania", icon: <Image src="/oceania-region.png" alt="North India" width={80} height={80} /> },
 ];
 
 export function RegionsSection() {
@@ -41,7 +41,7 @@ export function RegionsSection() {
           </h3>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
             {domesticRegions.map((region) => (
-              <Link href={`/home/search-result-with-filter?destinationTags=${region.name}`}>
+              <Link href={`/home/search-result-with-filter?destinationTags=${region.id}`}>
                 <RegionItem
                   key={region.name}
                   name={region.name}
@@ -59,7 +59,7 @@ export function RegionsSection() {
           </h3>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
             {internationalRegions.map((region) => (
-              <Link href={`/home/search-result-with-filter?destinationTags=${region.name}`}>
+              <Link href={`/home/search-result-with-filter?destinationTags=${region.id}`}>
                 <RegionItem
                   key={region.name}
                   name={region.name}
